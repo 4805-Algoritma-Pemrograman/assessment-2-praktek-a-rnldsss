fad10b9b55815216a7e795264c40d2ca4c51ed87
Raynaldi Paniroean Panjaitan <raynaldypand.20@gmail.com>
2024-12-12 08:32:28 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -2,47 +2,50 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Nomor2 {
-
-    static ArrayList<Integer> keranjangBelanja = new ArrayList<>();
+    
+    public static final int[] hargaMenu = {35000, 45000, 20000, 15000};
+    public static ArrayList<Integer> keranjangBelanja = new ArrayList<>();
 
     public static void main(String[] args) {
-        int[] hargaMenu = {35000, 45000, 20000, 15000};
-        String[] namaMenu = {"Burger", "Steak", "Spaghetti", "Kentang"};
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            String pilihan;
+            
+            do {
+                System.out.print("Masukkan nama menu [Burger/Steak/Spaghetti/French fries]: ");
+                String menu = input.nextLine();
 
-        while (true) {
-            System.out.print("Masukan nama menu [Burger/Steak/Spaghetti/Kentang]: ");
-            String pilihanMenu = input.next();
-            int indexMenu = -1;
-
-            for (int i = 0; i < namaMenu.length; i++) {
-                if (namaMenu[i].equals(pilihanMenu)) {
-                    indexMenu = i;
-                    break;
+                // Menentukan harga berdasarkan input pengguna
+                int harga = 0;
+                switch (menu.toLowerCase()) {
+                    case "burger":
+                        harga = hargaMenu[0];
+                        break;
+                    case "steak":
+                        harga = hargaMenu[1];
+                        break;
+                    case "spaghetti":
+                        harga = hargaMenu[2];
+                        break;
+                    case "french fries":
+                        harga = hargaMenu[3];
+                        break;
+                    default:
+                        System.out.println("Menu tidak tersedia.");
+                        continue; 
                 }
-            }
 
-            if (indexMenu != -1) {
-                keranjangBelanja.add(hargaMenu[indexMenu]);
-            } else {
-                System.out.println("Menu tidak tersedia");
-            }
-
-            System.out.print("Input lagi (Y/N)?: ");
-            String jawaban = input.next();
-
-            if (jawaban.equals("N")) {
-                break;
-            }
+                keranjangBelanja.add(harga);
+                
+                System.out.print("Input lagi (Y/N)?: ");
+                pilihan = input.nextLine();
+            } while (pilihan.equalsIgnoreCase("Y"));
         }
-
         int totalBelanja = hitungTotalBelanja();
-        int diskon = hitungDiskon(totalBelanja);
-        int totalBayar = totalBelanja - diskon;
+        double diskon = hitungDiskon(totalBelanja);
+        double totalBayar = totalBelanja - diskon;
 
-        System.out.println("Total item pesanan : " + keranjangBelanja.size());
-        System.out.println("Total yang harus dibayar : " + totalBayar);
-        System.out.println(Nomor2.class.getDeclaredMethods().length);
+        System.out.println("Total item pesanan = " + keranjangBelanja.size());
+        System.out.println("Total yang harus dibayar = " + totalBayar);
     }
 
     public static int hitungTotalBelanja() {
@@ -53,13 +56,13 @@ public class Nomor2 {
         return total;
     }
 
-    public static int hitungDiskon(int totalBelanja) {
-        if (totalBelanja < 50000) {
-            return 0;
-        } else if (totalBelanja >= 50000 && totalBelanja <= 100000) {
-            return (int) (totalBelanja * 0.05);
+    public static double hitungDiskon(int total) {
+        if (total < 50000) {
+            return 0; // Tidak ada diskon
+        } else if (total >= 50000 && total <= 100000) {
+            return total * 0.05; // Diskon 5%
         } else {
-            return (int) (totalBelanja * 0.1);
+            return total * 0.10; // Diskon 10%
         }
     }
 }
