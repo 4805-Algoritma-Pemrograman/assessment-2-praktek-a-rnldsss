09c041beaf47dc3b9c66b449f294524a4f6fa264
Raynaldi Paniroean Panjaitan <raynaldypand.20@gmail.com>
2024-12-12 08:46:08 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,22 +2,19 @@ import java.util.Scanner;
 
 public class Nomor1 {
 
-    public static void tampilkanAngkaGanjil(int N) {
-        System.out.println("Angka Ganjil");
-        for (int i = 0; i <= N; i++) {
-        if (i % 2 != 0) {
-            System.out.println(i + "");
-        }
-    }
-    System.out.println();
-}
-
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.print("Masukkan bilangan N: ");
         int N = input.nextInt();
         tampilkanAngkaGanjil(N);
-    
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
+    }
+
+    public static void tampilkanAngkaGanjil(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
     }
 }
