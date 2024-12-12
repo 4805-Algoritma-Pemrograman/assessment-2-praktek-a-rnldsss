ca70b22c9664b6154549b59794b680b70dcb5a66
Raynaldi Paniroean Panjaitan <raynaldypand.20@gmail.com>
2024-12-12 09:05:16 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -2,39 +2,45 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Nomor2 {
+    static int[] hargaMenu = {35000, 45000, 20000, 15000};
+    static ArrayList<Integer> keranjangBelanja = new ArrayList<>();
+    static ArrayList<String> menuMakanan = new ArrayList<>();
+
     public static void main(String[] args) {
+        menuMakanan.add("Burger");
+        menuMakanan.add("Steak");
+        menuMakanan.add("Spagetti");
+        menuMakanan.add("Kentang");
+
         Scanner scanner = new Scanner(System.in);
-
-        ArrayList<Integer> keranjangBelanja = new ArrayList<>();
-
-        while (true) {
-            System.out.println("Masukan nama menu [Burger/Steak/Spaghetti/Kentang]:");
-            String inputMenu = scanner.nextLine();
-
-            int indexMenu = getIndexMenu(inputMenu);
-            if (indexMenu != -1) {
-                keranjangBelanja.add(getHargaMenu(indexMenu));
+        String input;
+        do {
+            System.out.println("Daftar Menu:");
+            for (int i = 0; i < menuMakanan.size(); i++) {
+                System.out.println((i + 1) + ". " + menuMakanan.get(i));
             }
-
-            System.out.println("Input lagi (Y/N)?:");
-            String inputLagi = scanner.nextLine();
-
-            if (inputLagi.equalsIgnoreCase("N")) {
-                break;
+            System.out.print("Masukan nama menu (1-4): ");
+            input = scanner.nextLine();
+            int pilihan = Integer.parseInt(input);
+            if (pilihan >= 1 && pilihan <= 4) {
+                int harga = hargaMenu[pilihan - 1];
+                keranjangBelanja.add(harga);
+            } else {
+                System.out.println("Menu tidak tersedia. Silakan coba lagi.");
             }
-        }
+            System.out.print("Input lagi (Y/N)?: ");
+            input = scanner.nextLine();
+        } while (input.equalsIgnoreCase("Y"));
 
-        int totalBelanja = hitungTotalBelanja(keranjangBelanja);
-        int diskon = hitungDiskon(totalBelanja);
-        int totalBayar = totalBelanja - diskon;
+        int totalBelanja = hitungTotalBelanja();
+        double diskon = hitungDiskon(totalBelanja);
+        double totalBayar = totalBelanja - diskon;
 
-        System.out.println("Total item pesanan : " + keranjangBelanja.size());
-        System.out.println("Total yang harus dibayar : " + totalBayar);
-        System.out.println(keranjangBelanja.size());
+        System.out.println("Total item pesanan = " + keranjangBelanja.size());
+        System.out.println("Total yang harus dibayar = " + totalBayar);
     }
 
-    // Menghitung total belanja
-    private static int hitungTotalBelanja(ArrayList<Integer> keranjangBelanja) {
+    public static int hitungTotalBelanja() {
         int total = 0;
         for (int harga : keranjangBelanja) {
             total += harga;
@@ -42,36 +48,13 @@ public class Nomor2 {
         return total;
     }
 
-    // Menghitung diskon
-    private static int hitungDiskon(int totalBelanja) {
-        if (totalBelanja < 50000) {
-            return 0;
-        } else if (totalBelanja >= 50000 && totalBelanja <= 100000) {
-            return (int) (totalBelanja * 0.05);
-        } else {
-            return (int) (totalBelanja * 0.1);
+    public static double hitungDiskon(int totalBelanja) {
+        double diskon = 0;
+        if (totalBelanja >= 50000 && totalBelanja <= 100000) {
+            diskon = totalBelanja * 0.05;
+        } else if (totalBelanja > 100000) {
+            diskon = totalBelanja * 0.10;
         }
-    }
-
-    // Mendapatkan harga menu
-    private static int getHargaMenu(int indexMenu) {
-        int[] hargaMenu = {35000, 45000, 20000, 15000};
-        return hargaMenu[indexMenu];
-    }
-
-    // Mendapatkan index menu
-    private static int getIndexMenu(String inputMenu) {
-        switch (inputMenu) {
-            case "Burger":
-                return 0;
-            case "Steak":
-                return 1;
-            case "Spaghetti":
-                return 2;
-            case "Kentang":
-                return 3;
-            default:
-                return -1;
-        }
+        return diskon;
     }
 }
