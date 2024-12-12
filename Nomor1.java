73cf1305e978129949d8e1eaebbcb1913e7e2014
Raynaldi Paniroean Panjaitan <raynaldypand.20@gmail.com>
2024-12-12 08:47:15 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,20 +1,24 @@
 import java.util.Scanner;
 
 public class Nomor1 {
-
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        tampilkanAngkaGanjil(N);
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
-    }
-
-    public static void tampilkanAngkaGanjil(int n) {
+    public static void tampilkanBilanganGanjil(int n) {
+        System.out.print("Bilangan ganjil dari 0 hingga " + n + " adalah: ");
         for (int i = 0; i <= n; i++) {
             if (i % 2 != 0) {
                 System.out.print(i + " ");
             }
         }
-        System.out.println();
+        System.out.println(); 
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan bilangan n: ");
+        int n = input.nextInt();
+        
+        tampilkanBilanganGanjil(n);
+        
+        input.close();
     }
 }
